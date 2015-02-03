bdf0d3bf4e5a50311de5ecd82857aba217fb8779
Danny Thomas <dannyt@netflix.com>
2015-02-02 16:20:21 -0800

    Remove dependency on Guava to prevent build script classpath from being polluted with a version later than gradleApi() provides

--- src/main/groovy/nebula/plugin/publishing/xml/WithXmlAction.groovy
+++ src/main/groovy/nebula/plugin/publishing/xml/WithXmlAction.groovy
@@ -1,21 +1,19 @@
 package nebula.plugin.publishing.xml
 
-import com.google.common.base.Preconditions
-
 import java.util.logging.Logger
 
 class WithXmlAction {
     private static final Logger LOGGER = Logger.getLogger(WithXmlAction.getName())
 
-    private Node root
     private Closure closure
 
     WithXmlAction(Closure closure) {
-        this.closure = Preconditions.checkNotNull(closure, "Closure has to be set during constructor")
+        assert closure : "Closure has to be set during constructor"
+        this.closure = closure
     }
 
     def execute(Node root) {
-        Preconditions.checkNotNull(root)
+        assert root
 
         closure.delegate = new MissingPropertyToStringDelegate(root)
 
