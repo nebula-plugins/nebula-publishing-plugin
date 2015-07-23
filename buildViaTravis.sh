5759f58b35cdca24442d26c870aa85d75bd16321
Rob Spieldenner <rob.spieldenner@gmail.com>
2015-07-22 17:11:08 -0700

    Trying coveralls one more time

--- buildViaTravis.sh
+++ buildViaTravis.sh
@@ -3,21 +3,21 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  ./gradlew build jacocoTestReport coveralls
+  ./gradlew build
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
-  ./gradlew -Prelease.travisci=true snapshot jacocoTestReport coveralls
+  ./gradlew -Prelease.travisci=true snapshot
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" != "" ]; then
   echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
   case "$TRAVIS_TAG" in
   *-rc\.*)
-    ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true candidate jacocoTestReport coveralls
+    ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true candidate
     ;;
   *)
-    ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true final jacocoTestReport coveralls publishPlugins
+    ./gradlew -Prelease.travisci=true -Prelease.useLastTag=true final publishPlugins
     ;;
   esac
 else
   echo -e 'WARN: Should not be here => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']  Pull Request ['$TRAVIS_PULL_REQUEST']'
-  ./gradlew build jacocoTestReport coveralls
+  ./gradlew build
 fi
