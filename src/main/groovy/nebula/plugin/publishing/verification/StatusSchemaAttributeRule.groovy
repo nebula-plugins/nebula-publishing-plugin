44d86010b8406eccc777c5d993028dadcf235607
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2018-09-28 16:35:30 -0700

    Use ComponentMetadataRules for setting attributes

---
+++ src/main/groovy/nebula/plugin/publishing/verification/StatusSchemaAttributeRule.groovy
@@ -0,0 +1,21 @@
+package nebula.plugin.publishing.verification
+
+import org.gradle.api.artifacts.CacheableRule
+import org.gradle.api.artifacts.ComponentMetadataContext
+import org.gradle.api.artifacts.ComponentMetadataDetails
+import org.gradle.api.artifacts.ComponentMetadataRule
+
+
+@CacheableRule
+class StatusSchemaAttributeRule implements ComponentMetadataRule {
+    @Override
+    void execute(ComponentMetadataContext componentMetadataContext) {
+        modifyAttributes(componentMetadataContext.details)
+    }
+
+    static protected void modifyAttributes(ComponentMetadataDetails details) {
+        details.attributes {
+            attribute PublishVerificationPlugin.STATUS_SCHEME, details.statusScheme.join(',')
+        }
+    }
+}
