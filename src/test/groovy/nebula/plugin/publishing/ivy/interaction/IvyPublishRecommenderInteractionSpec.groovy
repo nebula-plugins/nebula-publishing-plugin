e18c9527cef9bae62510efde2ad47e158e3cc3d4
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-04-19 14:14:34 -0700

    Add test for java library plugin in IvyPublishRecommendationSpec

--- src/test/groovy/nebula/plugin/publishing/ivy/interaction/IvyPublishRecommenderInteractionSpec.groovy
+++ src/test/groovy/nebula/plugin/publishing/ivy/interaction/IvyPublishRecommenderInteractionSpec.groovy
@@ -7,7 +7,7 @@ import spock.lang.Unroll
 
 class IvyPublishRecommenderInteractionSpec extends IntegrationTestKitSpec {
     @Unroll
-    def 'dependencies from recommendation plugin in #scope scope should be in ivy file'() {
+    def 'dependencies from recommendation plugin in #scope scope should be in ivy file - java-library plugin'() {
         def graph = new DependencyGraphBuilder().addModule('test:foo:1.0.0').build()
         def generator = new GradleDependencyGenerator(graph, "$projectDir/repo")
         generator.generateTestMavenRepo()
@@ -59,4 +59,60 @@ class IvyPublishRecommenderInteractionSpec extends IntegrationTestKitSpec {
         scope << ['compile', 'implementation', 'api', 'runtimeOnly']
 
     }
+
+    @Unroll
+    def 'dependencies from recommendation plugin in #scope scope should be in ivy file - java plugin'() {
+        def graph = new DependencyGraphBuilder().addModule('test:foo:1.0.0').build()
+        def generator = new GradleDependencyGenerator(graph, "$projectDir/repo")
+        generator.generateTestMavenRepo()
+
+        settingsFile.text = """
+            rootProject.name='mytest'
+            """.stripIndent()
+
+        buildFile << """\
+            plugins {
+                id 'java'
+                id 'nebula.ivy-publish'
+                id 'nebula.dependency-recommender' version '5.0.0'
+            }
+            
+            group = 'test.nebula'
+            version = '0.1.0'
+            
+            repositories {
+                ${generator.mavenRepositoryBlock}
+            }
+
+            dependencyRecommendations {
+               map recommendations: ['test:foo': '1.0.0']
+            }
+
+            dependencies {
+                $scope 'test:foo'
+            }
+
+            publishing {
+                repositories {
+                    ivy {
+                        name 'testlocal'
+                        url 'build/testlocal'
+                    }
+                }
+            }
+            """.stripIndent()
+
+        when:
+        def results = runTasks('publishNebulaIvyPublicationToTestlocalRepository')
+
+        then:
+        def ivy = new XmlSlurper().parse(new File(projectDir, 'build/testlocal/test.nebula/mytest/0.1.0/ivy-0.1.0.xml'))
+        ivy.dependencies.dependency.first().@rev == '1.0.0'
+
+        where:
+        scope << ['compile', 'implementation', 'runtimeOnly']
+
+    }
+
+
 }
