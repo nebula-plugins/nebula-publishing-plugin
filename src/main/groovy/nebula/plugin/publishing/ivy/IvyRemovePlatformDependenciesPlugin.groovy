3297efc1be5cf24d7c59ebe75bca093d18563299
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-05-07 15:59:32 -0700

    add copyright and comments

--- src/main/groovy/nebula/plugin/publishing/ivy/IvyRemovePlatformDependenciesPlugin.groovy
+++ src/main/groovy/nebula/plugin/publishing/ivy/IvyRemovePlatformDependenciesPlugin.groovy
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2019 Netflix, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package nebula.plugin.publishing.ivy
 
 import org.gradle.api.Plugin
@@ -5,6 +21,10 @@ import org.gradle.api.Project
 import org.gradle.api.XmlProvider
 import org.gradle.api.publish.ivy.IvyPublication
 
+
+/**
+ * Removes from descriptor dependencies that have category status of platform or enhanced-platform
+ */
 class IvyRemovePlatformDependenciesPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
