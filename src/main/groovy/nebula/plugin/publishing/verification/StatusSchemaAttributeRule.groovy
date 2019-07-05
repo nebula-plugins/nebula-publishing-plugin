ac5de0abdec31ab8ecbcacabc561730835988df9
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2019-07-05 08:52:40 -0700

    direct invocation of attribure method

--- src/main/groovy/nebula/plugin/publishing/verification/StatusSchemaAttributeRule.groovy
+++ src/main/groovy/nebula/plugin/publishing/verification/StatusSchemaAttributeRule.groovy
@@ -5,19 +5,19 @@ import org.gradle.api.artifacts.CacheableRule
 import org.gradle.api.artifacts.ComponentMetadataContext
 import org.gradle.api.artifacts.ComponentMetadataDetails
 import org.gradle.api.artifacts.ComponentMetadataRule
-import org.gradle.api.attributes.Attribute
 
 
 //@CacheableRule TODO: this is disable to test RealisedMavenModuleResolveMetadataSerializationHelper duplicate objects
 @CompileStatic
 class StatusSchemaAttributeRule implements ComponentMetadataRule {
-    public static final Attribute<String> STATUS_SCHEME = Attribute.of('org.my.internal.statusScheme', String)
-
     @Override
     void execute(ComponentMetadataContext componentMetadataContext) {
-        ComponentMetadataDetails details = componentMetadataContext.details
+        modifyAttributes(componentMetadataContext.details)
+    }
+
+    static void modifyAttributes(ComponentMetadataDetails details) {
         details.attributes {
-            it.attribute STATUS_SCHEME, details.statusScheme.join(',')
+            it.attribute(PublishVerificationPlugin.STATUS_SCHEME, details.statusScheme.join(','))
         }
     }
 }
