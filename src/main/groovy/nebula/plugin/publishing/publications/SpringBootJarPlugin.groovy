8223d84d122f479a5b8b81f9d02fd67c74fa9a84
Roberto Perez Alcolea <rperezalcolea@netflix.com>
2020-02-03 13:59:37 -0800

    SpringBootJarPlugin should happen after project evaluation

--- src/main/groovy/nebula/plugin/publishing/publications/SpringBootJarPlugin.groovy
+++ src/main/groovy/nebula/plugin/publishing/publications/SpringBootJarPlugin.groovy
@@ -11,16 +11,19 @@ class SpringBootJarPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        project.plugins.withId('org.springframework.boot') {
-            project.configurations {
-                [apiElements, runtimeElements].each {
-                    Task bootJarTask = project.tasks.findByName('bootJar')
-                    if(bootJarTask) {
-                        it.outgoing.artifacts.removeIf { it.buildDependencies.getDependencies(null).contains(project.tasks.named('jar', Jar).get()) }
-                        it.outgoing.artifact(bootJarTask)
+        project.afterEvaluate {
+            project.plugins.withId('org.springframework.boot') {
+                project.configurations {
+                    [apiElements, runtimeElements].each {
+                        Task bootJarTask = project.tasks.findByName('bootJar')
+                        if(bootJarTask) {
+                            it.outgoing.artifacts.removeIf { it.buildDependencies.getDependencies(null).contains(project.tasks.named('jar', Jar).get()) }
+                            it.outgoing.artifact(bootJarTask)
+                        }
                     }
                 }
             }
         }
+
     }
 }
