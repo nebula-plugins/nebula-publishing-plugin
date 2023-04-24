9de46e7ba411a3fe4cb0dc3a9b02406ccbddf671
Danny Thomas <dannyt@netflix.com>
2023-04-24 12:17:34 +1000

    Avoid eagerly configuring the Spring bootJar task

--- src/main/groovy/nebula/plugin/publishing/publications/SpringBootJarPlugin.groovy
+++ src/main/groovy/nebula/plugin/publishing/publications/SpringBootJarPlugin.groovy
@@ -4,6 +4,8 @@ import groovy.transform.CompileDynamic
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.artifacts.Configuration
+import org.gradle.api.plugins.JavaPlugin
 import org.gradle.jvm.tasks.Jar
 
 @CompileDynamic
@@ -11,19 +13,23 @@ class SpringBootJarPlugin implements Plugin<Project> {
 
     @Override
     void apply(Project project) {
-        project.afterEvaluate {
-            project.plugins.withId('org.springframework.boot') {
+        project.plugins.withId('org.springframework.boot') {
+            project.plugins.withType(JavaPlugin) {
                 project.configurations {
-                    [apiElements, runtimeElements].each {
-                        Task bootJarTask = project.tasks.findByName('bootJar')
-                        if(bootJarTask) {
-                            it.outgoing.artifacts.removeIf { it.buildDependencies.getDependencies(null).contains(project.tasks.named('jar', Jar).get()) }
-                            it.outgoing.artifact(bootJarTask)
+                    [apiElements, runtimeElements].each { Configuration configuration ->
+                        def bootJar = project.tasks.named('bootJar')
+                        bootJar.configure {
+                            def outgoing = configuration.outgoing
+                            outgoing.artifacts.removeIf {
+                                it.buildDependencies.getDependencies(null)
+                                        .contains(project.tasks.named('jar', Jar).get())
+                            }
+                            outgoing.artifact(bootJar)
                         }
                     }
                 }
             }
         }
-
     }
+
 }
